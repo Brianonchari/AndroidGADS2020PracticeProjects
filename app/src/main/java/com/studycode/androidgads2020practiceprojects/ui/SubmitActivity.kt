ddc9355f6583337a6f495c0d6e0f91511aaad200
Brian_OB <25736452+Brianonchari@users.noreply.github.com>
2020-09-07 10:29:27 +0300

    alertdialogs

--- app/src/main/java/com/studycode/androidgads2020practiceprojects/ui/SubmitActivity.kt
+++ app/src/main/java/com/studycode/androidgads2020practiceprojects/ui/SubmitActivity.kt
@@ -1,31 +1,45 @@
 package com.studycode.androidgads2020practiceprojects.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.studycode.androidgads2020practiceprojects.R
 import com.studycode.androidgads2020practiceprojects.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_submit.*
+import kotlinx.android.synthetic.main.confirm_diallog.view.*
 
 @AndroidEntryPoint
 class SubmitActivity : AppCompatActivity() {
-
+    private val viewModel: MainViewModel by viewModels()
+//    private lateinit var binding: S
     companion object {
         private const val TAG = "SubmitActivity"
     }
 
-    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_submit)
 
         submitButton.setOnClickListener {
-            makeSubmission()
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.confirm_diallog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Title")
+
+            val mAlertDialog = mBuilder.show()
+            mDialogView.button.setOnClickListener {
+                makeSubmission()
+                mAlertDialog.dismiss()
+            }
+            mDialogView.imageView.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
         }
-
-
     }
 
     private fun makeSubmission() {
